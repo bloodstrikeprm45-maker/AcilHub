21edf641d0f4a569bb4233945f6be11ac247e0f6
AcilOfficial <bloodstrikeprm45@gmail.com>
2026-08-19 01:25:30 +0700

    Add files via upload

---
+++ Util/BloxFruitModule/Preload/helper.lua
@@ -0,0 +1,28 @@
+local Guard = (function()
+    local Players = game:GetService("Players")
+    local plr = Players.LocalPlayer
+    local PlayerGui = plr:WaitForChild("PlayerGui")
+    local BLOCKED_METHODS = {
+        Destroy = true,
+        Remove = true,
+        ClearAllChildren = true,
+    }
+    local OldNamecall
+    OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+        local method = getnamecallmethod()
+
+        if checkcaller() then
+            return OldNamecall(self, ...)
+        end
+        if typeof(self) == "Instance" and self == PlayerGui and BLOCKED_METHODS[method] then
+            task.defer(function()
+                warn(("[ Quantum Guard] Blocked '%s' on PlayerGui"):format(method))
+            end)
+            return nil
+        end
+        return OldNamecall(self, ...)
+    end))
+    task.defer(function()
+        print("[Guard] PlayerGui protection active")
+    end)
+end)()
